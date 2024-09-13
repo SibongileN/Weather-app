c6f433d8977bc957298ec04c8ba3451eef9acd21
SibongileN <nsibongile039@gmail.com>
2024-09-13 14:18:32 +0200

    Edited axios link on html and added js function for temprature

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-function searchCity(event){
+function searchSubmit(event){
     event.preventDefault();
     let searchInput= document.querySelector("#search-input");
-    let city = document.querySelector("#city");
+   
 
-    city.innerHTML= searchInput.value;
+    searchCity(searchInput.value);
 
 }
 
 let searchBar = document.querySelector("form");
-searchBar.addEventListener("submit",searchCity);
+searchBar.addEventListener("submit",searchSubmit);
 
 
+function displayWeather(response){
+    let currentTemperature = document.querySelector("#current-temperature");
+    let temperature = Math.round(response.data.temperature.current);
+    console.log("temperature");
+
+    let city = document.querySelector("#city");
+city.innerHTML= response.data.city;    
+
+    currentTemperature.innerHTML= `${temperature}`
+}
+function searchCity(city){
 let apiKey= "93704d2fc3113d491t780f96oa1cbb5b";
-let apiUrl= `https://api.shecodes.io/weather/v1/current?lon={lon}&lat=${city}&key=${apiKey}`;
-axios.get(apiUrl).then(refreshWeather)
+let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}=&key=${apiKey}`;
+axios.get(apiUrl).then(displayWeather)
+}
