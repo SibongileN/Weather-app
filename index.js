4fa3e9148ae6a4b8952f55f7b21b0ee17a4b58af
SibongileN <nsibongile039@gmail.com>
2024-09-13 12:21:19 +0200

    Added js function for the search engine

---
+++ index.js
@@ -0,0 +1,11 @@
+function searchCity(event){
+    event.preventDefault();
+    let searchInput= document.querySelector("#search-input");
+    let city = document.querySelector("#city");
+
+    city.innerHTML= searchInput.value;
+
+}
+
+let searchBar = document.querySelector("form");
+searchBar.addEventListener("submit",searchCity);
